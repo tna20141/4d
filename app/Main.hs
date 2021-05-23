d027087d1c9a02e8175b99f85897d8fec258a25c
anhlq <anhlq2110@gmail.com>
2021-05-23 16:25:13 +0700

    ss

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,76 @@
 module Main where
 
 import Lib
-import Graphics.Gloss
 import Numeric.LinearAlgebra.HMatrix
 
+
+import Graphics.Rendering.OpenGL as GL
+import Graphics.UI.GLFW as GLFW
+import Control.Monad (forever)
+import System.Exit (exitSuccess)
+
+keyPressed :: GLFW.KeyCallback
+keyPressed win GLFW.Key'Escape _ GLFW.KeyState'Pressed _ = shutdown win
+keyPressed _   _               _ _                     _ = return ()
+
+shutdown :: GLFW.WindowCloseCallback
+shutdown win =
+  do
+    GLFW.destroyWindow win
+    GLFW.terminate
+    _ <- exitSuccess
+    return ()
+
+resizeWindow :: GLFW.WindowSizeCallback
+resizeWindow _ w h =
+  do
+    GL.viewport   $= (GL.Position 0 0, GL.Size (fromIntegral w) (fromIntegral h))
+    GL.matrixMode $= GL.Projection
+    GL.loadIdentity
+    GL.ortho2D 0 (realToFrac w) (realToFrac h) 0
+
+openWindow :: String -> (Int, Int) -> IO GLFW.Window
+openWindow title (sizex,sizey) =
+  do
+    GLFW.init
+    GLFW.defaultWindowHints
+    GLFW.windowHint (GLFW.WindowHint'ContextVersionMajor 4)
+    GLFW.windowHint (GLFW.WindowHint'ContextVersionMinor 5)
+    GLFW.windowHint (GLFW.WindowHint'OpenGLProfile GLFW.OpenGLProfile'Core)
+    GLFW.windowHint (GLFW.WindowHint'Resizable False)
+    Just win <- GLFW.createWindow sizex sizey title Nothing Nothing
+    GLFW.makeContextCurrent (Just win)
+    GLFW.setWindowSizeCallback win (Just resizeWindow)
+    GLFW.setKeyCallback win (Just keyPressed)
+    GLFW.setWindowCloseCallback win (Just shutdown)
+    return win
+
+closeWindow :: GLFW.Window -> IO ()
+closeWindow win =
+  do
+    GLFW.destroyWindow win
+    GLFW.terminate
+
+display :: IO ()
+display =
+  do
+    inWindow <- openWindow "Hello Window!" (512,512)
+    onDisplay inWindow
+    closeWindow inWindow
+
+onDisplay :: GLFW.Window -> IO ()
+onDisplay win =
+  do
+    GL.clearColor $= Color4 1 0 0 1
+    GL.clear [ColorBuffer]
+    GLFW.swapBuffers win
+
+    GLFW.pollEvents
+    onDisplay win
+
+---------------------------------------------------------------------------
+
 main :: IO ()
-main
- = display
-        (InWindow
-               "Hello World"     -- window title
-                (400, 150)       -- window size
-                (10, 10))        -- window position
-        white                    -- background color
-        picture                  -- picture to display
-
-txt :: String
-txt = show (euclidianDistance (makePoint 1 2 3 4) (makePoint 5 6 7 8))
-
-
-
-picture :: Picture
-picture
-        = Translate (-170) (-20) -- shift the text to the middle of the window
-        $ Scale 0.5 0.5          -- display it half the original size
-        $ Text txt     -- text to display
+main =
+  do
+    display
